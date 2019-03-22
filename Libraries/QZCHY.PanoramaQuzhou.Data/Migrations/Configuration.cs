26e91dc54b650adb3b97074217c1aed05414ab36
Dai <Dai@DESKTOP-0EJH4SR>
2019-03-22 11:16:35 +0800

    补回Configuration

---
+++ Libraries/QZCHY.PanoramaQuzhou.Data/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace QZCHY.PanoramaQuzhou.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<QZCHY.PanoramaQuzhou.Data.PanoramaQuzhuoObjectContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(QZCHY.PanoramaQuzhou.Data.PanoramaQuzhuoObjectContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
